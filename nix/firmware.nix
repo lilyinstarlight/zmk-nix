25bef69fb5a28b618288ac0c7385b44c3c4b0245
Lily Foster <lily@lily.flowers>
2023-12-01 19:21:44 -0500

    config,nix: bump deps

--- nix/firmware.nix
+++ nix/firmware.nix
@@ -11,7 +11,7 @@ buildSplitKeyboard {
   board = "nice_nano_v2";
   shield = "sofle_rgb_keyhive_%PART% nice_view_adapter nice_view";
 
-  zephyrDepsHash = "sha256-G1hxea7V4sY2j30V8iTjn8c6aXzAhiXmHdfgJ3wTsTw=";
+  zephyrDepsHash = "sha256-tjjd8du+Rf+AM0XnwkDMik+T599v7R7oXI+rl8bvBSw=";
 
   meta = with lib; {
     description = "Keyboard firmware for Sofle RGB, Keyhive variant with nice!view screens";
