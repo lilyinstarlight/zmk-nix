e141a62d5d143c23438c154066b4aaa3718558b6
Lily Foster <lily@lily.flowers>
2023-12-04 16:32:02 -0500

    nix/update: generalize a bit more

--- nix/update.nix
+++ nix/update.nix
@@ -1,6 +1,10 @@
 { lib
 , writeShellApplication
+, coreutils
+, findutils
 , git
+, gnugrep
+, gnused
 , nix
 , yq
 }:
@@ -9,7 +13,11 @@ writeShellApplication {
   name = "sofle-firmware-update";
 
   runtimeInputs = [
+    coreutils
+    findutils
     git
+    gnugrep
+    gnused
     nix
     yq
   ];
@@ -21,7 +29,8 @@ writeShellApplication {
     toplevel="$(git rev-parse --show-toplevel || (printf 'Could not find root of repository\nAre we running from within the git repo?\n' >&2; exit 1))"
 
     # get package attr and path
-    attr="firmware"
+    attr="''${UPDATE_NIX_ATTR_PATH:-firmware}"
+    westRoot="$toplevel/''${UPDATE_WEST_ROOT:-config}"
     pkgpath="$(nix eval --raw "$toplevel"#"$attr".meta.position | cut -d: -f1)"
     outpath="$(nix eval --raw --impure --expr "builtins.fetchGit { url = \"$toplevel\"; shallow = true; }")"
     [ -n "$outpath" ] && pkgpath="''${pkgpath/$outpath/$toplevel}"
@@ -33,21 +42,21 @@ writeShellApplication {
       | .manifest.projects
       | map(.remote as $remote | {name, url: (($remotes[] | select(.name == $remote))."url-base" + "/" + (if ."repo-path" then ."repo-path" else .name end)), revision}) []
       | tostring + "\u0000"
-    ' "$toplevel"/config/west.yml | xargs -0 -L1 bash -c '
+    ' "$westRoot"/west.yml | xargs -0 -L1 "$BASH" -c '
       set -euo pipefail
 
-      toplevel="$0"
+      westRoot="$0"
       project="$1"
 
       url="$(echo "$project" | yq -r .url)"
       currevision="$(echo "$project" | yq -r .revision)"
-      head="$(grep -F "$currevision" "$toplevel"/config/west.yml | cut -d"#" -f2 | tr -d " ")"
+      head="$(grep -F "$currevision" "$westRoot"/west.yml | cut -d"#" -f2 | tr -d " ")"
 
       [ -z "$head" ] && exit 0
 
       newrevision="$(git ls-remote "$url" "$head" | sed -e "s/\t.*$//")"
-      sed -i -e "s|$currevision|$newrevision|" "$toplevel"/config/west.yml
-    ' "$toplevel"
+      sed -i -e "s|$currevision|$newrevision|" "$westRoot"/west.yml
+    ' "$westRoot"
 
     # get new deps hash
     curhash="$(nix eval --raw "$toplevel"#"$attr".westDeps.outputHash)"
