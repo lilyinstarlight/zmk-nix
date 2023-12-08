3908221ea5947c27f3f774ced63a4c4f1addc0bd
Lily Foster <lily@lily.flowers>
2023-12-07 22:39:59 -0500

    nix/firmware,readme: fix actual sofle rgb variant

--- nix/firmware.nix
+++ nix/firmware.nix
@@ -9,12 +9,12 @@ buildSplitKeyboard {
   src = lib.sourceFilesBySuffices self [ ".conf" ".keymap" ".yml" ];
 
   board = "nice_nano_v2";
-  shield = "sofle_rgb_keyhive_%PART% nice_view_adapter nice_view";
+  shield = "sofle_%PART% nice_view_adapter nice_view";
 
   zephyrDepsHash = "sha256-fvTgvpn9Cp96ePJxlAzUn5+ewszVwRJdVsYBZZ+uG40=";
 
   meta = with lib; {
-    description = "Keyboard firmware for Sofle RGB, Keyhive variant with nice!view screens";
+    description = "Keyboard firmware for Sofle RGB with nice!view screens";
     license = licenses.mit;
     platforms = platforms.all;
     maintainers = with maintainers; [ lilyinstarlight ];
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # zmk-nix
 
-Build system and configuration files for generating and building ZMK firmware to use on my Sofle RGB (Keyhive variant)
+Build system and configuration files for generating and building ZMK firmware to use on my Sofle RGB
 
 
 ## Builders
