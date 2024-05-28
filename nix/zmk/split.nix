6aa67cbf39078b02fc014152efbd38f7533e5468
Lily Foster <lily@lily.flowers>
2024-05-28 12:04:46 -0400

    nix/zmk: add support for %PART% substitution on board

--- nix/zmk/split.nix
+++ nix/zmk/split.nix
@@ -16,8 +16,9 @@ let
 in runCommand name ({
   inherit parts westDeps;
 } // args // (lib.genAttrs parts (part:
-  buildKeyboard ((lib.attrsets.removeAttrs args [ "shield" "parts" ]) // {
+  buildKeyboard ((lib.attrsets.removeAttrs args [ "board" "shield" "parts" ]) // {
     name = "${name}-${part}";
+    board = lib.replaceStrings [ "%PART%" ] [ part ] board;
     shield = lib.replaceStrings [ "%PART%" ] [ part ] shield;
     westDeps = args.westDeps or westDeps;
   })
