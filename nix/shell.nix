e46fc6e944e5f2e99164d754486ab49c0f8affc0
Lily Foster <lily@lily.flowers>
2024-06-27 10:50:26 -0400

    nix/shell: add recommended upstream packages for tooling and other west commands

--- nix/shell.nix
+++ nix/shell.nix
@@ -8,7 +8,22 @@
 mkShell {
   packages = [
     cmake ninja
-    (python3.withPackages (ps: [ ps.west ps.pyelftools ]))
+    (python3.withPackages (ps: [
+      # From https://github.com/zmkfirmware/zephyr/blob/HEAD/scripts/requirements-base.txt
+      ps.west
+      ps.pyelftools
+      ps.pyyaml
+      ps.pykwalify
+      ps.canopen
+      ps.packaging
+      ps.progress
+      ps.psutil
+      ps.pylink-square
+      ps.pyserial
+      ps.requests
+      ps.anytree
+      ps.intelhex
+    ]))
     gcc-arm-embedded
   ];
 
