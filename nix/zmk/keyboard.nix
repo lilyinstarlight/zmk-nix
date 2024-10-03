b7ce9a3b1daf95b6aa223e0ac1e6457c1b3fcc00
Lily Foster <lily@lily.flowers>
2024-10-03 11:14:54 -0400

    nix/zmk: add enableZmkStudio

--- nix/zmk/keyboard.nix
+++ nix/zmk/keyboard.nix
@@ -1,6 +1,8 @@
 { lib
 , buildZephyrPackage
 , runCommand
+, protobuf
+, python3
 }:
 
 { board
@@ -11,19 +13,31 @@
 , config ? "config"
 , extraWestBuildFlags ? []
 , extraCmakeFlags ? []
-, ... } @ args: buildZephyrPackage ((lib.attrsets.removeAttrs args [ "config" "extraWestBuildFlags" "extraCmakeFlags" ]) // {
+, enableZmkStudio ? false
+, ... } @ args: (buildZephyrPackage.override { inherit python3; }) ((lib.attrsets.removeAttrs args [ "config" "extraWestBuildFlags" "extraCmakeFlags" "enableZmkStudio" ]) // {
   inherit name;
+
+  nativeBuildInputs = lib.optionals enableZmkStudio [
+    protobuf
+    python3.pythonOnBuildForHost.pkgs.protobuf
+    python3.pythonOnBuildForHost.pkgs.grpcio-tools
+  ] ++ (args.nativeBuildInputs or []);
 
   westRoot = config;
 
   westBuildFlags = [
     "-s" "zmk/app"
     "-b" board
-  ] ++ extraWestBuildFlags ++ [
+  ] ++ extraWestBuildFlags ++ lib.optionals enableZmkStudio [ "-S" "studio-rpc-usb-uart" ] ++ [
     "--"
   ] ++ lib.optional (shield != null) "-DSHIELD=${shield}" ++ extraCmakeFlags;
 
   preConfigure = ''
     westBuildFlagsArray+=("-DZMK_CONFIG=$(readlink -f ${lib.escapeShellArg config})")
+
+    if [ -d modules/lib/nanopb/generator ]; then
+      chmod +x modules/lib/nanopb/generator/{nanopb_generator,protoc,protoc-gen-nanopb}
+      patchShebangs modules/lib/nanopb/generator
+    fi
   '';
 })
