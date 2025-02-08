6c87f2b76d74d9f11be04a7738e061f876046da8
Lily Foster <lily@lily.flowers>
2025-02-08 17:32:06 -0500

    nix/zmk: only set enableZmkStudio on central part

--- nix/zmk/split.nix
+++ nix/zmk/split.nix
@@ -7,20 +7,23 @@
 , board
 , shield ? null
 , parts ? [ "left" "right" ]
+, centralPart ? (lib.head parts)
+, enableZmkStudio ? false
 , ... } @ args:
 
 let
-  westDeps = args.westDeps or (buildKeyboard ((lib.removeAttrs args [ "parts" ]) // {
+  westDeps = args.westDeps or (buildKeyboard ((lib.removeAttrs args [ "parts" "centralPart" ]) // {
     inherit name;
   })).westDeps;
 in runCommand name ((lib.removeAttrs args [ "zephyrDepsHash" "westDeps" "westRoot" "config" "enableZmkStudio" "extraWestBuildFlags" "extraCmakeFlags" ]) // {
-  inherit board shield parts westDeps;
+  inherit board shield parts centralPart westDeps;
   inherit (westDeps) westRoot;
 } // (lib.genAttrs parts (part:
-  buildKeyboard ((lib.removeAttrs args [ "board" "shield" "parts" ]) // {
+  buildKeyboard ((lib.removeAttrs args [ "board" "shield" "parts" "centralPart" "enableZmkStudio" ]) // {
     name = "${name}-${part}";
     board = lib.replaceStrings [ "%PART%" ] [ part ] board;
     shield = if shield != null then lib.replaceStrings [ "%PART%" ] [ part ] shield else shield;
+    enableZmkStudio = if part == centralPart then enableZmkStudio else false;
     inherit westDeps;
   })
 ))) ''
